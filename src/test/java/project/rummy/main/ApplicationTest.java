29bbd5a1aeaa245d67c1daff658fa909a9a45d6c
Anna Shi <annashi@cmail.carleton.ca>
2018-09-27 12:17:45 -0400

    runnable unit test

--- src/test/java/project/rummy/main/ApplicationTest.java
+++ src/test/java/project/rummy/main/ApplicationTest.java
@@ -1,7 +1,11 @@
 package project.rummy.main;
 
-import static org.junit.Assert.*;
-
+import org.junit.jupiter.api.*;
 public class ApplicationTest {
+    @Test
+    @DisplayName("this to to test commits")
+    public void TestCase(){
+        System.out.print("???");
+    }
 
 }
