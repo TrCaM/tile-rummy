26ea5d4662c773d271ace58eeffef48361fcf307
Anna Shi <annashi@cmail.carleton.ca>
2018-09-30 19:52:06 -0400

    demo merge conflict

---
+++ src/main/java/project/rummy/main/test.java
@@ -0,0 +1,8 @@
+package project.rummy.main;
+
+public final class test {
+    // remove
+    static String c = "please remove this later";
+    static String red = "demo code";
+    static String mr_potatoe = "Can not eat potatoe";
+}
