9310e4c84557167cb0a214d0892f35273818be12
Anna Shi <annashi@cmail.carleton.ca>
2018-09-30 20:20:43 -0400

    this should result in conflict

---
+++ src/main/java/project/rummy/main/test.java
@@ -0,0 +1,12 @@
+package project.rummy.main;
+
+public class test {
+    public test() {
+        super();
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
