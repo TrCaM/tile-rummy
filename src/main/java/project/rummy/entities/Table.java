e097fdff470198f4b482512e845a49bb42bf6817
Anna Shi <annashi@cmail.carleton.ca>
2018-10-08 01:10:47 -0400

    This the implementation of the InitTable, with functions shuffle seperate and a private create deck which would indicate how many decks are used... for this it is 2.

--- src/main/java/project/rummy/entities/Table.java
+++ src/main/java/project/rummy/entities/Table.java
@@ -1,5 +1,7 @@
 package project.rummy.entities;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -8,13 +10,32 @@ import java.util.List;
 public class Table {
   private List<Tile> freeTiles;
   private List<Meld> melds;
+  private int MAX_VALUE = 13;
+  private int DECKS_AMOUNT = 2;
 
-  /**
-   * Generate all the tiles needed for the game and shuffle them randomly
-   * TODO: Write tests and implement
-   */
+
+
   public void initTiles() {
-    throw new UnsupportedOperationException();
+      freeTiles = new ArrayList<>();
+      createDeck();
+  }
+  public void shuffle() {
+      Collections.shuffle(freeTiles);
+  }
+
+  private void createDeck() {
+    for (int j = 0; j < DECKS_AMOUNT; j++) {
+        for (Color color : Color.values()) {
+            for (int i = 1; i <= MAX_VALUE; i++) {
+                this.freeTiles.add(Tile.createTile(color, i));
+            }
+        }
+    }
+  }
+
+
+  public List<Tile> getFreeTiles(){
+    return this.freeTiles;
   }
 
   public void addMeld(Meld meld) {
