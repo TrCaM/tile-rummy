fe2457531a02d452f7094c1baaa5feb8c8e49b10
TrCaM <cmtshcb@gmail.com>
2018-10-14 11:21:44 -0400

    Code: Implement createMeld method of Meld.java

--- src/main/java/project/rummy/entities/Meld.java
+++ src/main/java/project/rummy/entities/Meld.java
@@ -10,70 +10,68 @@ public class Meld {
   private MeldType type;
   private MeldSource source;
 
+  private Meld(Tile tile) {
+    this.tiles = Collections.singletonList(tile);
+    this.type = MeldType.SINGLE;
+    this.source = MeldSource.HAND;
+  }
+
   private Meld(List<Tile> tiles, MeldType type) {
     this.tiles = tiles;
     this.type = type;
+    this.source = MeldSource.HAND;
   }
 
   /**
    * Create a meld or a portion of meld by grouping a list of tiles. The creation is valid when:
-   *  + There's only tile in the meld: It is a SINGLE meld tyle
-   *  + There's two tiles in the meld: isValidMeldPart should return true, but not isValidMeld
-   *  + There's three tiles in the meld: isValidMeldPart and isValidMeld both return true.
+   * + There's only tile in the meld: It is a SINGLE meld tyle
+   * + There's two tiles in the meld: isValidMeldPart should return true, but not isValidMeld
+   * + There's three tiles in the meld: isValidMeldPart and isValidMeld both return true.
    */
-  public static Meld createMeld(Tile ...tiles) {
-    /* @TODO: write tests and implement this method to that it can detect the MeldType */
-    // return new Meld(Arrays.asList(tiles), MeldType.RUN);
-    if(tiles.length < 3 ){
-      throw new IllegalArgumentException("Not enough tiles");
+  public static Meld createMeld(Tile... tiles) {
+    if (tiles.length == 0) {
+      throw new IllegalArgumentException("Invalid tiles input");
+    }
+    if (tiles.length == 1) {
+      return new Meld(tiles[0]);
     }
     if (isRun(tiles)) {
-      return new Meld (Arrays.asList(tiles), MeldType.RUN);
+      return new Meld(Arrays.asList(tiles), MeldType.RUN);
     } else if (isSet(tiles)) {
-      return new Meld (Arrays.asList(tiles), MeldType.SET);
+      return new Meld(Arrays.asList(tiles), MeldType.SET);
     }
     throw new IllegalArgumentException("Invalid tiles input");
   }
 
-  //Check if a meld is a RUN
+  /**
+   * Check if a meld is a RUN
+   */
   private static boolean isRun(Tile[] tiles) {
-      //Initialize first tile colour and value to compare
+    //Initialize first tile colour and value to compare
     Color color = tiles[0].color();
     int startValue = tiles[0].value();
     //Check if two consecutive tiles have the same colour and increasing in value
-    for(int i = 0; i < tiles.length; i++){
-      if (tiles[i].color() != color || tiles[i].value() != startValue + i){
+    for (int i = 0; i < tiles.length; i++) {
+      if (tiles[i].color() != color || tiles[i].value() != startValue + i) {
         return false;
       }
     }
     return true;
   }
 
-  //Check if a meld is a SET
+  /**
+   * Check if a meld is a SET
+   */
   private static boolean isSet(Tile[] tiles) {
-      //Check for existing colours in a meld (no duplicate)
-    Set noDup = new HashSet();
-    for(int i = 0; i < tiles.length; i ++){
-      noDup.add(tiles[i].color());
-    }
-
-    //Something wrong here
-      // I'm trying to check if there is existing 2 tiles with the same colour and value
-    for(int j = 0; j < tiles.length; j++ ){
-      if((noDup.contains(tiles[j].color()) && noDup.contains(tiles[j + 1].color()))
-              && (tiles[j].value() == tiles[j-1].value())){
+    //Check for existing colours in a meld (no duplicate)
+    Set<Color> existedColors = new HashSet<>();
+    int value = tiles[0].value();
+    for (Tile tile : tiles) {
+      if (!existedColors.add(tile.color()) || tile.value() != value) {
         return false;
       }
     }
-      return true;
-    }
-
-  /**
-   * Check if this is a valid partial meld.
-   */
-  private boolean isValidMeldPart() {
-    // TODO: Write tests and implement the method
-    throw new UnsupportedOperationException();
+    return true;
   }
 
   /**
