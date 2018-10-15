ece082811e649045aedf979606d1bd936949c712
quanghienle <Hien1308>
2018-10-15 01:29:39 -0400

    Test: create tests for split() method in ManipulationTable class

---
+++ src/test/java/project/rummy/entities/ManipulationTableTest.java
@@ -0,0 +1,103 @@
+package project.rummy.entities;
+
+import org.junit.Test;
+
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+import static org.junit.Assert.*;
+
+
+public class ManipulationTableTest {
+
+    private static final Tile O5 = Tile.createTile(Color.ORANGE, 5);
+    private static final Tile O6 = Tile.createTile(Color.ORANGE, 6);
+    private static final Tile O7 = Tile.createTile(Color.ORANGE, 7);
+    private static final Tile O8 = Tile.createTile(Color.ORANGE, 8);
+    private static final Tile O9 = Tile.createTile(Color.ORANGE, 9);
+    private static final Tile R3 = Tile.createTile(Color.RED, 3);
+    private static final Tile G3 = Tile.createTile(Color.GREEN, 3);
+    private static final Tile B3 = Tile.createTile(Color.BLACK, 3);
+    private static final Tile O3 = Tile.createTile(Color.ORANGE, 3);
+
+    @Test(expected = IllegalArgumentException.class)
+    public void split_invalidBreakpoint_shouldThrow() {
+        ManipulationTable table = new ManipulationTable();
+
+        table.add(Meld.createMeld(O5, O6, O7, O8, O9), Meld.createMeld(R3, G3, B3, O3));
+        table.split(0, 5);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void split_duplicateBreakpoint_shouldThrow() {
+        ManipulationTable table = new ManipulationTable();
+
+        table.add(Meld.createMeld(O5, O6, O7, O8, O9), Meld.createMeld(R3, G3, B3, O3));
+        table.split(0, 2, 2);
+    }
+
+
+
+    @Test
+    public void split_Run_WithSingleBreakPoint(){
+        ManipulationTable table = new ManipulationTable();
+
+        table.add(Meld.createMeld(O5, O6, O7, O8, O9), Meld.createMeld(R3, G3, B3, O3));
+        table.split(0, 3);
+
+        assertThat(table.getMelds().get(0).tiles(), contains(R3, G3, B3, O3));
+        assertThat(table.getMelds().get(1).tiles(), contains(O5, O6, O7));
+        assertThat(table.getMelds().get(2).tiles(), contains(O8, O9));
+    }
+
+    @Test
+    public void split_Set_WithSingleBreakPoint(){
+        ManipulationTable table = new ManipulationTable();
+
+        table.add(Meld.createMeld(O5, O6, O7, O8, O9), Meld.createMeld(R3, G3, B3, O3));
+        table.split(1, 3);
+
+        assertThat(table.getMelds().get(0).tiles(), contains(O5, O6, O7, O8, O9));
+        assertThat(table.getMelds().get(1).tiles(), contains(R3, G3, B3));
+        assertThat(table.getMelds().get(2).tiles(), contains(O3));
+    }
+
+    @Test
+    public void split_Run_With2BreakPoints(){
+        ManipulationTable table = new ManipulationTable();
+
+        table.add(Meld.createMeld(O5, O6, O7, O8, O9), Meld.createMeld(R3, G3, B3, O3));
+        table.split(0, 2, 4);
+
+        assertThat(table.getMelds().get(0).tiles(), contains(R3, G3, B3, O3));
+        assertThat(table.getMelds().get(1).tiles(), contains(O5, O6));
+        assertThat(table.getMelds().get(2).tiles(), contains(O7, O8));
+        assertThat(table.getMelds().get(3).tiles(), contains(O9));
+    }
+
+    @Test
+    public void split_Set_With2BreakPoints(){
+        ManipulationTable table = new ManipulationTable();
+
+        table.add(Meld.createMeld(O5, O6, O7, O8, O9), Meld.createMeld(R3, G3, B3, O3));
+        table.split(1, 1, 2);
+
+        assertThat(table.getMelds().get(0).tiles(), contains(O5, O6, O7, O8, O9));
+        assertThat(table.getMelds().get(1).tiles(), contains(R3));
+        assertThat(table.getMelds().get(2).tiles(), contains(G3));
+        assertThat(table.getMelds().get(3).tiles(), contains(B3, O3));
+    }
+
+    @Test
+    public void split_RunAndSet(){
+        ManipulationTable table = new ManipulationTable();
+
+        table.add(Meld.createMeld(O5, O6, O7, O8, O9), Meld.createMeld(R3, G3, B3, O3));
+        table.split(0, 1, 4);
+        table.split(0, 2);
+
+        assertThat(table.getMelds().get(0).tiles(), contains(O5));
+        assertThat(table.getMelds().get(1).tiles(), contains(O6, O7, O8));
+        assertThat(table.getMelds().get(2).tiles(), contains(O9));
+        assertThat(table.getMelds().get(3).tiles(), contains(R3, G3));
+        assertThat(table.getMelds().get(4).tiles(), contains(B3, O3));
+    }
+}
