0a65f9c14decff056342a3552a0231d4942bf705
TrCaM <cmtshcb@gmail.com>
2018-10-28 12:42:42 -0400

    Code: implement startGame method

--- src/main/java/project/rummy/game/Game.java
+++ src/main/java/project/rummy/game/Game.java
@@ -1,5 +1,7 @@
 package project.rummy.game;
 
+import project.rummy.commands.CommandProcessor;
+import project.rummy.control.ActionHandler;
 import project.rummy.entities.Table;
 import project.rummy.entities.Player;
 import project.rummy.observers.Observable;
@@ -14,6 +16,7 @@ public class Game implements Observable {
   private int currentPlayer;
   private List<Observer> observers;
   private int turnNumber;
+  private CommandProcessor commandProcessor;
 
   Game(Player[] players, Table table) {
     this.players = players;
@@ -21,6 +24,7 @@ public class Game implements Observable {
     this.observers = new ArrayList<>();
     this.turnNumber = 0;
     this.currentPlayer = 0;
+    this.commandProcessor = CommandProcessor.getInstance();
   }
 
   /**
@@ -31,6 +35,13 @@ public class Game implements Observable {
    *  + Constantly check the state of the game and check for when the game should end
    */
   public void start() {
+    while(!isGameEnd()) {
+      currentPlayer = (currentPlayer + 1) % 4;
+      commandProcessor.setUpHandler(new ActionHandler(players[currentPlayer], table));
+      players[currentPlayer].getController().playTurn();
+      currentPlayer = -1;
+    }
+    // TODO: Display dialog to show the winner and done the game.
   }
 
   /**
@@ -39,10 +50,15 @@ public class Game implements Observable {
    *  + When there's no free tile to draw.
    * @return the player index who won the game. That player either is the only player who has no
    *         tile or has the least points in hand when there's no tile to draw and play.
+   *         Return -1 if the game is not ended.
    */
-  public int isGameEnd() {
+  public int getWinner() {
     //TODO: Write test and implement the method
     throw new UnsupportedOperationException();
+  }
+
+  public boolean isGameEnd() {
+    return getWinner() != -1;
   }
   Table getTable() {
     return this.table;
