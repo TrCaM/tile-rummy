e83038d0792fd3c1db59420e55e503926e55a13f
Anna Shi <annashi@cmail.carleton.ca>
2018-10-28 20:35:01 -0400

    Removed the default observer code, due to imcompatability and will be using custom code.

--- src/main/java/project/rummy/main/Run.java
+++ src/main/java/project/rummy/main/Run.java
@@ -4,7 +4,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import project.rummy.Observer.TableSource;
 import project.rummy.entities.Table;
 import project.rummy.gui.GUIController;
 import javax.swing.*;
@@ -17,13 +16,6 @@ public class Run extends javafx.application.Application {
 
   @Override
   public void start(Stage primaryStage) throws Exception{
-      System.out.println("Enter Text: ");
-    TableSource tableSource = new TableSource();
-    tableSource.addObserver(((o, arg) -> {
-      System.out.println("This ran " + arg);
-    }));
-
-    new Thread(tableSource).start();
 
     GUIController gui = new GUIController();
     gui.StartMenu(primaryStage);
