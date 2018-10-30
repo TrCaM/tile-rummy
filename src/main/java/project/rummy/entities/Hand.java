ae01a4f8b8357a8e44763881f97dc1c5fc622862
quanghienle <Hien1308>
2018-10-30 00:44:36 -0400

    code: implement findPossibleSets() method in Hand class

--- src/main/java/project/rummy/entities/Hand.java
+++ src/main/java/project/rummy/entities/Hand.java
@@ -74,8 +74,8 @@ public class Hand {
    */
   public void sort() {
     tiles.sort((tile1, tile2) -> tile1.color() != tile2.color()
-        ? tile1.color().compareTo(tile2.color())
-        : tile1.value() - tile2.value());
+            ? tile1.color().compareTo(tile2.color())
+            : tile1.value() - tile2.value());
   }
 
   /**
@@ -95,4 +95,37 @@ public class Hand {
     }
     return sum;
   }
+
+  /**
+   * find all possible sets that can be made up from a list of tiles
+   */
+  public List<Meld> findPossibleSets(List<Tile> tilesList){
+
+    List<Meld> possibleMelds = new ArrayList<>();
+    List<Tile> alist = new ArrayList<>(tilesList);
+
+    while(alist.size() > 2) {
+      List<Tile> tempMeld = new ArrayList<>();
+      tempMeld.add(alist.get(0));
+
+      for (int i = 1; i < alist.size(); i++) {
+        if (alist.get(0).value() == alist.get(i).value()
+                && alist.get(0).color() != alist.get(i).color()
+                && !tempMeld.contains(alist.get(i))) {
+          tempMeld.add(alist.get(i));
+        }
+      }
+
+      if (tempMeld.size() >= 3) {
+        possibleMelds.add(Meld.createMeld(tempMeld));
+      }
+      for(Tile e: tempMeld){
+        alist.remove(alist.indexOf(e));
+      }
+    }
+    return possibleMelds;
+  }
+
+
+
 }
