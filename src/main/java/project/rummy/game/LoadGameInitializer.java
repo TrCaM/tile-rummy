040ab050dc4573342bf043744e5fc34870e5fd3e
MikeMNguyen <minhmnguyen@cmail.carleton.ca>
2018-10-31 11:10:42 -0400

    Added empty table

--- src/main/java/project/rummy/game/LoadGameInitializer.java
+++ src/main/java/project/rummy/game/LoadGameInitializer.java
@@ -7,26 +7,31 @@ import project.rummy.entities.Table;
  * Initialize game from the predefined game state. Useful for testing
  * TODO: Write tests and implement all the methods
  */
-public class LoadGameInitializer implements GameInitializer{
-  private GameState state;
+public class LoadGameInitializer implements GameInitializer {
+    private GameState state;
 
-  public LoadGameInitializer(GameState state) {
-    this.state = state;
-  }
+    public LoadGameInitializer(GameState state) {
+        this.state = state;
+    }
 
-  @Override
-  public void initPlayers(Game game) {
-    throw new UnsupportedOperationException();
-  }
+    @Override
+    public void initPlayers(Game game) {
+        throw new UnsupportedOperationException();
+    }
 
-  @Override
-  public void initTable(Game game) {
-    throw new UnsupportedOperationException();
-  }
+    @Override
+    public void initTable(Game game) {
+        throw new UnsupportedOperationException();
+    }
 
-  @Override
-  public void initializeGameState(Player[] players, Table table) {
-    /* TODO: We can actually left this empty if you already do everything else before */
-    throw new UnsupportedOperationException();
-  }
+    @Override
+    public void initEmptyTable(Game game) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void initializeGameState(Player[] players, Table table) {
+        /* TODO: We can actually left this empty if you already do everything else before */
+        throw new UnsupportedOperationException();
+    }
 }
