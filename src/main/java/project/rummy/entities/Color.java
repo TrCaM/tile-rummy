cba5994ec2010bc25037e5bdbf1d5a7a4c6aff1d
quanghienle <Hien1308>
2018-10-31 17:49:05 -0400

    refactor: fixing color value

--- src/main/java/project/rummy/entities/Color.java
+++ src/main/java/project/rummy/entities/Color.java
@@ -4,19 +4,14 @@ package project.rummy.entities;
  * An enum to indicate different card colors
  */
 public enum Color {
-  RED {
-    public int value = 0;
-  }, BLACK {
-    public int value = 1;
-  }, GREEN {
-    public int value = 2;
-  }, ORANGE {
-    public int value = 3;
-  };
+  RED(0), BLACK(1), GREEN(2), ORANGE(3);
 
   private int value;
 
   public int value() {
     return this.value;
+  }
+  private Color(int value){
+    this.value =value;
   }
 }
