80c036936d0df96984db299d953ad95f9c7d2f5e
quanghienle <Hien1308>
2018-11-01 11:21:07 -0400

    new class to store player's name and controller type

---
+++ src/main/java/project/rummy/entities/PlayerData.java
@@ -0,0 +1,20 @@
+package project.rummy.entities;
+
+import project.rummy.control.Controller;
+
+public class PlayerData {
+    public String controllerType;
+    public String name;
+
+    /**
+     *
+     * @param name
+     * @param controller: human, strategy1, strategy2, strategy3
+     */
+    PlayerData(String name, String controller){
+        this.name = name;
+        this.controllerType = controller;
+    }
+
+
+}
