43c0e7f13af00b144f9b21fe36b5cfd71e50597f
quanghienle <Hien1308>
2018-11-02 17:21:34 -0400

    test: for formMeldByDetaching()

--- src/test/java/project/rummy/ai/CombinationSeekerTest.java
+++ src/test/java/project/rummy/ai/CombinationSeekerTest.java
@@ -27,6 +27,11 @@ public class CombinationSeekerTest {
     private static final Tile G5 = Tile.createTile(Color.GREEN, 5);
     private static final Tile B5 = Tile.createTile(Color.BLACK, 5);
 
+    private static final Tile R11 = Tile.createTile(Color.RED, 11);
+    private static final Tile G11 = Tile.createTile(Color.GREEN, 11);
+    private static final Tile B11 = Tile.createTile(Color.BLACK, 11);
+    private static final Tile O11 = Tile.createTile(Color.ORANGE, 11);
+
 
     private List<Meld> melds;
 
@@ -36,7 +41,7 @@ public class CombinationSeekerTest {
     }
 
     @Test
-    public void formingSet_test() {
+    public void formSet_test() {
         melds.add(Meld.createMeld(O4, O5, O6, O7, O8, O9));
         melds.add(Meld.createMeld(R4, G4, B4, O4));
         melds.add(Meld.createMeld(G5, B5, O5));
@@ -61,7 +66,30 @@ public class CombinationSeekerTest {
 
         Map<Meld, Integer> map3 = CombinationSeeker.formSet(5, Color.ORANGE, melds);
         assertEquals(0, map3.size());
+    }
 
 
+    @Test
+    public void formRunByDetaching_test() {
+        melds.add(Meld.createMeld(O4, O5, O6, O7, O8, O9));
+        melds.add(Meld.createMeld(R4, G4, B4, O4));
+        melds.add(Meld.createMeld(R11, G11, B11, O11));
+        melds.add(Meld.createMeld(G5, B5, O5));
+
+        Map<Meld, Integer> map = CombinationSeeker.formRunByDetaching(10, Color.ORANGE, melds);
+
+        assertEquals(2,map.size());
+        assertTrue(map.containsKey(melds.get(0)));
+        assertTrue(map.containsKey(melds.get(2)));
+        assertTrue(map.get(melds.get(2))==3);
+        assertTrue(map.get(melds.get(0))==5);
+
+        Map<Meld, Integer> map2 = CombinationSeeker.formRunByDetaching(10, Color.RED, melds);
+        assertEquals(1,map2.size());
+        assertTrue(map2.get(melds.get(2))==0);
+
+        Map<Meld, Integer> map3 = CombinationSeeker.formRunByDetaching(5, Color.ORANGE, melds);
+        assertEquals(1, map3.size());
+        assertTrue(map3.get(melds.get(0))==0);
     }
 }
