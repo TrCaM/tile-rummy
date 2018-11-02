f67acefed20b013a2f5940a18f29690f4c8aa298
quanghienle <Hien1308>
2018-11-02 17:22:13 -0400

    code: implement formRunByDetaching()

--- src/main/java/project/rummy/ai/CombinationSeeker.java
+++ src/main/java/project/rummy/ai/CombinationSeeker.java
@@ -23,7 +23,7 @@ public class CombinationSeeker {
     public static Map<Meld, Integer> formSet(int tileValue, Color tileColor, List<Meld> tableMelds){
         List<Meld> copyMelds = new ArrayList<>(tableMelds);
 
-        //key is meld id and value is tile index
+        //key is meld and value is tile index
         Map<Meld, Integer> map = new HashMap<>();
 
         for(Color c: Color.values()){
@@ -43,7 +43,92 @@ public class CombinationSeeker {
         return map;
     }
 
-    public static void formRun(int tileValue, Color tileColor, List<Meld> tableMelds){
+    /**
+     *
+     * @param tileValue
+     * @param tileColor
+     * @param tableMelds
+     * @return
+     */
+    public static Map<Meld, Integer> formRunByDetaching(int tileValue, Color tileColor, List<Meld> tableMelds) {
+
+        List<Meld> copyMelds = new ArrayList<>(tableMelds);
+        //key is meld and value is tile index
+        Map<Meld, Integer> map = new HashMap<>();
+
+        int rightValue = tileValue +1;
+        while(rightValue <= 13) {
+            int meldid = TableMeldSeeker.findDetachableIdenticalTile(rightValue, tileColor, copyMelds);
+            if (meldid != 0) {
+                Meld m = Meld.getMeldFromId(meldid, copyMelds);
+                for (int i = 0; i < m.tiles().size(); i++) {
+                    if (m.tiles().get(i).value() == rightValue && m.tiles().get(i).color() == tileColor) {
+                        map.put(m, i);
+                        copyMelds.remove(copyMelds.indexOf(m));
+                    }
+                }
+                rightValue ++;
+            }else{
+                break;
+            }
+        }
+
+        int leftValue = tileValue - 1;
+        while(leftValue >= 1) {
+            int meldid = TableMeldSeeker.findDetachableIdenticalTile(leftValue, tileColor, copyMelds);
+            if (meldid != 0) {
+                Meld m = Meld.getMeldFromId(meldid, copyMelds);
+                for (int i = 0; i < m.tiles().size(); i++) {
+                    if (m.tiles().get(i).value() == leftValue && m.tiles().get(i).color() == tileColor) {
+                        map.put(m, i);
+                        copyMelds.remove(copyMelds.indexOf(m));
+                    }
+                }
+                leftValue --;
+            }else{
+                break;
+            }
+        }
+
+        return map;
+    }
+
+    /**
+     *
+     * @param tileValue
+     * @param tileColor
+     * @param tableMelds
+     */
+    public static void formRunGeneral(int tileValue, Color tileColor, List<Meld> tableMelds){
+
+        int rightId = TableMeldSeeker.findRightDetachableTiles(tileValue, tileColor, tableMelds);
+        Meld rightMeld = rightId == 0 ? null : Meld.getMeldFromId(rightId, tableMelds);
+
+        if(rightMeld != null){
+            for(int i=0; i<rightMeld.tiles().size(); i++){
+                if(rightMeld.tiles().get(i).value()== tileValue && rightMeld.tiles().get(i).color() == tileColor){
+                    //this is the number of tiles that we can detach
+                    if(rightMeld.tiles().size() - i - 1 >= 2){
+                        //TODO return something
+                    }
+                }
+            }
+        }
+
+        int leftId = TableMeldSeeker.findRightDetachableTiles(tileValue, tileColor, tableMelds);
+        Meld leftMeld = leftId == 0 ? null : Meld.getMeldFromId(leftId, tableMelds);
+
+        if(leftMeld != null){
+            for(int i=0; i<leftMeld.tiles().size(); i++){
+                if(leftMeld.tiles().get(i).value()== tileValue && leftMeld.tiles().get(i).color() == tileColor){
+                    //this is the number of tiles that we can detach
+                    if(i >= 2){
+                        //TODO return something
+                    }
+                }
+            }
+        }
+
 
     }
 }
