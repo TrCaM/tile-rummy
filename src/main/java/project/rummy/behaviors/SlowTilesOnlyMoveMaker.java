c0cee449020c49e309d2cf14b93b022252da1ed7
quanghienle <Hien1308>
2018-11-02 20:05:58 -0400

    new class: to play all available tiles only if other player has 3 or more tiles less than current player

---
+++ src/main/java/project/rummy/behaviors/SlowTilesOnlyMoveMaker.java
@@ -0,0 +1,34 @@
+package project.rummy.behaviors;
+
+import project.rummy.commands.Command;
+import project.rummy.game.GameState;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SlowTilesOnlyMoveMaker {
+    @Override
+    public List<Command> calculateMove(GameState state) {
+        List<Command> commands = new ArrayList<>();
+        List<Command> receivedCommands = new ArrayList<>();
+
+        ComputerMoveMaker move = new PlaySingleTileMoveMaker();
+        receivedCommands = move.calculateMove(state);
+        if(receivedCommands.isEmpty()){
+            commands.add(handler -> handler.draw());
+            return commands;
+        }
+
+        for(int i=0; i<4 && i!= state.getCurrentPlayer(); i++) {
+            while(state.getHandsData()[state.getCurrentPlayer()].tiles.size() - state.getHandsData()[i].tiles.size() >= 3){
+                if(!receivedCommands.isEmpty()){
+                    commands.addAll(receivedCommands);
+                    receivedCommands = move.calculateMove(state);
+                }
+            }
+        }
+
+        return commands;
+    }
+
+}
