f6ef788a062982fbcd89f232c76ec2783e4f9115
TrCaM <cmthscb@gmail.com>
2018-11-28 10:40:24 -0500

    Code: Starting of the game now works

--- src/main/java/project/rummy/game/GameStore.java
+++ src/main/java/project/rummy/game/GameStore.java
@@ -10,7 +10,6 @@ public class GameStore {
     this.initializer = initializer;
   }
 
-
   public Game initializeGame() {
     Game game = new Game(initializer instanceof NetworkGameInitializer);
     initializer.initPlayers(game);
@@ -18,13 +17,4 @@ public class GameStore {
     initializer.initializeGameState(game.getPlayers(), game.getTable());
     return game;
   }
-
-  public Game initializeGameStart() {
-    Game game = new Game();
-    initializer.initPlayers(game);
-    initializer.initTable(game);
-    initializer.initializeGameState(game.getPlayers(), game.getTable());
-    return game;
-  }
-
 }
