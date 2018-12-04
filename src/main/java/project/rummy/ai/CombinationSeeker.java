2aa3b46b8e35784fe7202a438afc1c77f9e9087c
quanghienle <quanghien.a1@gmail.com>
2018-12-03 23:54:31 -0500

    more complex board-reuse test cases

--- src/main/java/project/rummy/ai/CombinationSeeker.java
+++ src/main/java/project/rummy/ai/CombinationSeeker.java
@@ -5,25 +5,22 @@ import project.rummy.entities.Meld;
 import project.rummy.entities.MeldType;
 import project.rummy.entities.Tile;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class CombinationSeeker {
   private List<Tile> handTiles;
   private List<Meld> tableMelds;
 
-  public CombinationSeeker(List<Tile> handTiles, List<Meld> tableMelds){
+  public CombinationSeeker(List<Tile> handTiles, List<Meld> tableMelds) {
     this.handTiles = handTiles;
-    this.tableMelds  = tableMelds;
+    this.tableMelds = tableMelds;
   }
 
   /**
    * find all possible melds from which tiles can be detached
    * and form a new set with given tiles
    */
-  public  Map<Meld, Integer> formSet(List<Tile> collectedTiles) {
+  public Map<Meld, Integer> formSet(List<Tile> collectedTiles) {
     List<Meld> copyMelds = new ArrayList<>(tableMelds);
     List<Tile> remainingTiles = HandMeldSeeker.findRemainingTiles(handTiles);
     int tileValue = collectedTiles.get(0).value();
@@ -33,17 +30,17 @@ public class CombinationSeeker {
     Map<Meld, Integer> map = new HashMap<>();
 
     for (Color c : Color.values()) {
-      if (c != Color.ANY && c!= tileColor) {
+      if (c != Color.ANY && c != tileColor) {
         boolean tileInHand = false;
-        for(Tile t: remainingTiles){
-          if(t.canFillToRun(c, tileValue) && !collectedTiles.contains(t)){
+        for (Tile t : remainingTiles) {
+          if (t.canFillToRun(c, tileValue) && !collectedTiles.contains(t)) {
             collectedTiles.add(t);
             tileInHand = true;
             break;
           }
         }
 
-        if(!tileInHand) {
+        if (!tileInHand) {
           int id = TableMeldSeeker.findDetachableIdenticalTile(tileValue, c, copyMelds);
           if (id != 0) {
             Meld m = Meld.getMeldFromId(id, copyMelds);
@@ -70,11 +67,11 @@ public class CombinationSeeker {
   }
 
 
-  private int fillToRun(List<Tile> collectedTiles, int value, Color color, List<Meld> melds){
+  private int fillToRun(List<Tile> collectedTiles, int value, Color color, List<Meld> melds) {
     List<Tile> remainingTiles = HandMeldSeeker.findRemainingTiles(handTiles);
 
-    for(Tile t: remainingTiles){
-      if(t.canFillToRun(color,value) && !collectedTiles.contains(t)){
+    for (Tile t : remainingTiles) {
+      if (t.canFillToRun(color, value) && !collectedTiles.contains(t)) {
         collectedTiles.add(t);
         return 0;
       }
@@ -86,7 +83,7 @@ public class CombinationSeeker {
    * find all possible melds from which tiles can be detached
    * and form a new run with given tiles
    */
-  public  Map<Meld, Integer> formRunByDetaching(List<Tile> collectedTiles) {
+  public Map<Meld, Integer> formRunByDetaching(List<Tile> collectedTiles) {
     List<Meld> copyMelds = new ArrayList<>(tableMelds);
 
     Color tileColor = collectedTiles.get(0).color();
@@ -103,26 +100,26 @@ public class CombinationSeeker {
       int meldid = 0;
       int neededValue = 0;
 
-      hasRight = rightValue<=13;
-      hasLeft = leftValue >=1;
+      hasRight = rightValue <= 13;
+      hasLeft = leftValue >= 1;
 
       if (hasRight) {
         int previousSize = collectedTiles.size();
-        meldid = fillToRun(collectedTiles,rightValue,tileColor, copyMelds);
-        if(meldid == 0 && collectedTiles.size()-previousSize==0 ){
+        meldid = fillToRun(collectedTiles, rightValue, tileColor, copyMelds);
+        if (meldid == 0 && collectedTiles.size() - previousSize == 0) {
           hasRight = false;
-        }else{
+        } else {
           neededValue = rightValue;
-          rightValue ++;
+          rightValue++;
         }
       }
 
       if (hasLeft && !hasRight) {
         int previousSize = collectedTiles.size();
-        meldid = fillToRun(collectedTiles,leftValue,tileColor, copyMelds);
-        if(meldid == 0 && collectedTiles.size()-previousSize==0 ){
+        meldid = fillToRun(collectedTiles, leftValue, tileColor, copyMelds);
+        if (meldid == 0 && collectedTiles.size() - previousSize == 0) {
           hasLeft = false;
-        }else{
+        } else {
           neededValue = leftValue;
           leftValue--;
         }
@@ -131,10 +128,10 @@ public class CombinationSeeker {
       if (meldid != 0) {
         Meld m = Meld.getMeldFromId(meldid, tableMelds);
         copyMelds.remove(m);
-        for (int i = 0; i < m.tiles().size(); i++) {
-          if (m.tiles().get(i).canFillToRun(tileColor, neededValue)) {
-            map.put(m, i);
-            if (!m.tiles().get(i).isJoker()) {
+        for (Tile t : m.tiles()) {
+          if (t.canFillToRun(tileColor, neededValue)) {
+            map.put(m, m.tiles().indexOf(t));
+            if (m.tiles().indexOf(t) == 0 || m.tiles().indexOf(t) == m.tiles().size() - 1) {
               break;
             }
           }
@@ -146,20 +143,25 @@ public class CombinationSeeker {
 
   }
 
-  public  Map<Meld, Integer> formRunBySplitRight(int tileValue, Color tileColor) {
+  public Map<Meld, Integer> formRunBySplitRight(int tileValue, Color tileColor) {
 
     Map<Meld, Integer> map = new HashMap<>();
     int rightId = TableMeldSeeker.findRightDetachableTiles(tileValue, tileColor, tableMelds);
     Meld rightMeld = rightId == 0 ? null : Meld.getMeldFromId(rightId, tableMelds);
 
     if (rightMeld != null) {
-      for (int i = 0; i < rightMeld.tiles().size(); i++) {
-        if (rightMeld.tiles().get(i).value() == tileValue && rightMeld.tiles().get(i).color() == tileColor) {
-          //this is the number of tiles that we can detach
-          if (rightMeld.tiles().size() - i - 1 >= 2) {
-            map.put(rightMeld, i);
-            return map;
-          }
+      for (Tile t : rightMeld.tiles()) {
+
+        List<Tile> temp = new ArrayList<>(rightMeld.tiles());
+        temp.sort(Comparator.comparing(Tile::value));
+        int firstValue = temp.get(0).value() - rightMeld.tiles().indexOf(temp.get(0));
+        Color actualColor = temp.get(0).color();
+        int actualValue = firstValue + rightMeld.tiles().indexOf(t);
+        if (actualColor==tileColor && tileValue == actualValue &&
+            rightMeld.tiles().size() - rightMeld.tiles().indexOf(t) - 1 >= 2) {
+          map.put(rightMeld, rightMeld.tiles().indexOf(t));
+          return map;
+
         }
       }
     }
